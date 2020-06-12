00ffe37b36f3db288bb87261b6d879de0e31f0a4
483759 <483759@naver.com>
2020-06-12 07:38:22 -0700

    Change openfile() function return type

--- server.c
+++ server.c
@@ -22,57 +22,61 @@ void keycontrol(int sig);
 void *recvGetRequest(void *arg);
 void responseHTTP(int* sock, char* file, size_t* size, int type);
 char* getClientInfo(char* file, char* cli_ip, int* port_num, int* type);
-char* openFile(char* file, int* type);
+int openFile(char* file);
 void saveLogfile(char* cli_ip, char* file, int size);
 
 int main(int argc, char *argv[]){
-	int n,port_num;
-	char buffer[BUFSIZE];
-	pthread_t tid[THREADSIZE], recv;
-	struct sockaddr_in cli, sin;
-	FILE* log_fp;
-	int clientlen=sizeof(cli);
+	int n,port_num;						//Port number
+	pthread_t tid[THREADSIZE], recv;	//Thread for multiple client
+	struct sockaddr_in cli, sin;		//Web socket
+	FILE* log_fp;						//"log.txt" file
+	int clientlen=sizeof(cli);			//Client length
 
 	if(argc<3){
-		//perror(argc);
+		// argument error
 		printf("No sufficient argument\n");
 		exit(1);
 	}
 	
-	dir = (char*)malloc(sizeof(char)*BUFSIZE);
-	svc = (char*)malloc(sizeof(char)*BUFSIZE);
+	dir = (char*)malloc(sizeof(char)*BUFSIZE);	//current directory(server)
+	svc = (char*)malloc(sizeof(char)*BUFSIZE);	//service directory(argv[1])
 	dir = getcwd(NULL, BUFSIZ);
 	int i;
+	/*
 	for(i=0;argv[1][i+1]!='\0';i++)
 		argv[1][i]=argv[1][i+1];
 	argv[1][i]='\0';
-	chdir(argv[1]);
+	*/
+	chdir(argv[1]);				//move to service dir
 	svc = getcwd(NULL, BUFSIZE);
-	port_num = atoi(argv[2]);
+	printf("Service Directory = %s\n", svc);
+	port_num = atoi(argv[2]);		//assign port number
 	//save server directory and port num
 
 	log_fp = fopen("log.txt", "w");
 	if(log_fp==NULL)
 		perror("log_fp");
 	fclose(log_fp);
+	//initialize existing content by opening "log.txt" with "w" option
 
 	printf("server directory = %s\nPort num = %d\n\n",dir,port_num);
 	
 	if((sd = socket(AF_INET, SOCK_STREAM, 0))==-1){
 		perror("socket");
 		exit(1);
-	}
+	}	//socket create
 	memset((char*)&sin, '\0', sizeof(sin));
 	signal(SIGINT, keycontrol);
 
 	sin.sin_family=AF_INET;
 	sin.sin_port=htons(port_num);
 	sin.sin_addr.s_addr=inet_addr("0.0.0.0");
+	//assign socket information
 
 	if(bind(sd,(struct sockaddr*)&sin, sizeof(sin))==-1){
 		perror("bind");
 		exit(1);
-	}
+	}	//binding socket
 
 	if(listen(sd,5)==-1){	//wait client connect
 		perror("listen");
@@ -82,7 +86,7 @@ int main(int argc, char *argv[]){
 	if(pthread_mutex_init(&m_lock, NULL)!=0){
 		perror("Mutex Init Failure");
 		exit(1);
-	}
+	}	//mutex initilize for multiple client
 
 	while(1){	//server process
 		ns[t_size]=(int*)malloc(sizeof(int));
@@ -97,7 +101,7 @@ int main(int argc, char *argv[]){
 		pthread_mutex_unlock(&m_lock);
 
 		pthread_create(&recv, NULL, recvGetRequest, (void*) ns[t_size]);
-
+		//Call function with creating a thread
 		t_size++;
 	}
 
@@ -106,6 +110,7 @@ int main(int argc, char *argv[]){
 		close(*ns[t_size]);
 	close(sd);
 	fclose(log_fp);
+	//Close sockets and files
 	return 0;
 }
 
@@ -121,6 +126,7 @@ void keycontrol(int sig){
 }
 
 void *recvGetRequest(void *arg){
+	//To handle when a new access request is received on the server
 	int *sock = (int*)arg;
 	char buffer[BUFSIZE];
 	char cli_ip[20];
@@ -129,16 +135,14 @@ void *recvGetRequest(void *arg){
 
 	while(str_len>=0){
 		char* file_name=NULL;
-		int type;
-		if((str_len=recv(*sock, buffer, BUFSIZE, 0))==-1){
-			//printf("No Receive");
+		int type=-1;
+		if((str_len=recv(*sock, buffer, BUFSIZE, 0))==-1)
 			return (void*) NULL;
-		}
-
 		buffer[str_len]='\0';
+		//Save client request in buffer
 		
 		file_name = getClientInfo(buffer, cli_ip, &port_num, &type);
-		openFile(file_name, &type);
+		type = openFile(file_name);
 		responseHTTP(sock, file_name, &size, type);
 		saveLogfile(cli_ip, file_name, (int)size);
 		close(*sock);
@@ -146,10 +150,12 @@ void *recvGetRequest(void *arg){
 }
 
 void responseHTTP(int* sock, char* file, size_t* size, int type){
+	//Responds http depending on type
 	FILE* fp=NULL;
 	char content[BUFSIZE];
 	char respHeader[BUFSIZE];
 	char *dir;
+	printf("\ntype: %d\n",type);
 
 	if(type==-1){	//if the page isn't exist
 		strcpy(respHeader, "HTTP/1.1 404 Not Found\r\n\r\n"); 
@@ -168,9 +174,10 @@ void responseHTTP(int* sock, char* file, size_t* size, int type){
 				f=atoi(p);
 			}
 			p=strtok(NULL, "?=&");
-		}
+		}	//Tokenize each number from and to behind
 		for(int i=s;i<=f;i++)
 			sum+=i;
+		//"from" to "to" and stored in sum
 
 		strcpy(respHeader, "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n"); 
 		send(*sock, respHeader, strlen(respHeader),0);
@@ -179,6 +186,7 @@ void responseHTTP(int* sock, char* file, size_t* size, int type){
 		sprintf(content, "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">\r\n<html>\r\n<head>\r\n<title>total.cgi</title>\r\n<meta http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-1\">\r\n</head>\r\n<body>\r\n<h1>sum of %d to %d = %d</h1>\r\n</body>\r\n</html>\r\n", s, f, sum);
 		//Total.cgi html content 
 		send(*sock, content, strlen(content), 0);
+		//request content
 		return;
 	}
 
@@ -193,8 +201,9 @@ void responseHTTP(int* sock, char* file, size_t* size, int type){
 	*size = ftell(fp);
 	printf("file size: %d\n",(int)*size);
 	fseek(fp, 0, SEEK_SET);
+	//calculate file size
 
-	if(type==0){	//if file is .html
+	if(type==0){	//if file type is .html
 		strcpy(respHeader, "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n"); 
 		puts(respHeader);
 		write(*sock, respHeader, strlen(respHeader));
@@ -202,13 +211,14 @@ void responseHTTP(int* sock, char* file, size_t* size, int type){
 		while(fgets(content, BUFSIZE, fp)){
 			//puts(content);
 			send(*sock, content, strlen(content), 0);
-		}
+		}	//Read the contents of the html file one by one and respond 
 	}
 	else if(type==1){	//if the file is image
 		size_t fsize=0, nsize=0;
 		strcpy(respHeader, "HTTP/1.1 200 OK\r\nContent-Type: image/webp; charset=utf-8\r\n\r\n"); 
 		puts(respHeader);
 		write(*sock, respHeader, strlen(respHeader));
+		//Setting header to image type
 		
 		while(nsize != *size){
 			memset(content, 0x00, sizeof(content));
@@ -216,23 +226,24 @@ void responseHTTP(int* sock, char* file, size_t* size, int type){
 			nsize+= fsize;
 			//puts(content);
 			send(*sock, content, fsize, 0);
-		}
+		}	//Respond by reading the binary file as much as BUFSIZE
 
 	}
 	fclose(fp);
 }
 
-char* openFile(char* file, int* type){
+int openFile(char* file){
 	//Open file and image
+	//Read the path of the file to find the type of path
 	
 	FILE* fp = NULL;
 	char content[BUFSIZE];
 	char buffer[BUFSIZE];
+	int type=-1;
 	
 	if(strncmp(file,"total.cgi",9)==0){
 		//If the link is total.cgi -> type = 2
-		*type=2;
-		return NULL;
+		return 2;
 	}
 
 
@@ -241,22 +252,20 @@ char* openFile(char* file, int* type){
 		fp=fopen("index.html","r");
 		if(fp==NULL){	//if "index.html" doesn't exist, Not found
 			printf("Not found");
-			*type=-1;
-			return NULL;
+			return -1;
 		}
-		*type=0;
 		strcpy(file, "index.html");
-		return "index.html";
+		return 0;
 	}else
-		*type=0;
+		type = 0;
 
 	if(strncmp(file+strlen(file)-4, ".gif",4)==0
 			||strncmp(file+strlen(file)-4, ".jpg",4)==0){
-			*type=1;
+			type=1;
 	}	//image file
 	
 	fclose(fp);
-	return NULL; 
+	return type; 
 }
 
 char* getClientInfo(char* file, char* cli_ip, int* port_num, int* type){
